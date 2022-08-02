84aa0097a11de0757785225c9784c7e061263eb2
Willyam Pimentel <pimentelwillyam@gmail.com>
2022-08-02 15:04:31 -0300

    api funcionando sem es6

---
+++ src/index.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+// importando .env
+require("dotenv-safe").config({ silent: true });
+var express = require("express");
+var cors = require("cors");
+var app = express();
+app.get('/', function (req, res) {
+    res.status(200).send({ message: "Hello" });
+});
+app.use(cors({
+    origin: ["".concat(process.env.LOCALHOST)]
+}));
+app.listen(process.env.PORT, function () {
+    console.log("Escutando na porta ".concat(process.env.PORTA));
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("olá mundo!")
+// importando .env
+require("dotenv-safe").config({silent: true});
+
+//importando core da api
+import {Request,Response} from "express";
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.get('/', ( req: Request, res: Response) =>{
+    res.status(200).send({message: "Hello"})
+})
+
+app.use(cors({
+    origin: [`${process.env.LOCALHOST}`]
+}))
+
+app.listen(process.env.PORT, () =>{
+    console.log(`Escutando na porta ${process.env.PORTA}`)
+})
+
+
