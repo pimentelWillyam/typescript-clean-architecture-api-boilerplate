371fa3f226762e668ab2771ed746aedf58180299
GTI/WILLYAM.PIMENTEL <10482656417@sdspe.infra>
2022-10-27 14:09:13 -0300

    feat: removendo .js

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var app_1 = require("./api/app");
-app_1.app.listen(process.env.PORTA, function () {
-    console.log("aplica\u00E7\u00E3o iniciada na porta ".concat(process.env.PORTA));
-});
